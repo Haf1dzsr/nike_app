848b6a958c68c1ae67c1707706176c03a6fe7d96
Haf1dzsr <hsramadhan68@gmail.com>
2023-07-21 16:39:55 +0700

    add: onboarding screen and transition

--- lib/infrastructure/navigation/bindings/controllers/controllers_bindings.dart
+++ lib/infrastructure/navigation/bindings/controllers/controllers_bindings.dart
@@ -1,2 +1,3 @@
 export 'package:nike_app/infrastructure/navigation/bindings/controllers/home_controller.binding.dart'; 
+export 'package:nike_app/infrastructure/navigation/bindings/controllers/onboarding.controller.binding.dart'; 
 export 'package:nike_app/infrastructure/navigation/bindings/controllers/splash.controller_binding.dart'; 
--- lib/presentation/screens.dart
+++ lib/presentation/screens.dart
@@ -1,2 +1,3 @@
 export 'package:nike_app/presentation/home/home_screen.dart';
+export 'package:nike_app/presentation/onboarding/onboarding.screen.dart';
 export 'package:nike_app/presentation/splash/splash_screen.dart';
